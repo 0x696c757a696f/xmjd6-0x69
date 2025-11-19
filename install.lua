4d1b7b872bd029d42e1e2cb4fa5643da31950139
0x696c757a696f <39948247+0x696c757a696f@users.noreply.github.com>
2025-11-19 23:06:55 +0100

    Add schema and download functionality for xmjd6

--- install.lua
+++ install.lua
@@ -1,32 +1,33 @@
 local M = {}
 
 function M.xmjd6(ctx)
-    ctx:register_schema("xmjd6")
-    ctx:download_file("rime.lua", "rime.lua")
+
+    ctx:download_file("xmjd6.schema.yaml", "xmjd6.schema.yaml")
+    
+
+    ctx:download_dir("lua", "lua")
+
+
+    ctx:download_dir("opencc", "opencc")
+
+
+    local dicts = {
+       "xmjd6.extended.dict.yaml", 
+       "xmjd6.cx.dict.yaml",
+       "xmjd6.gbk.dict.yaml",
+       "liangfen.dict.yaml",
+       "xmjd6.Y.dict.yaml",
+       "xmjd6.Z.dict.yaml",
+       "xmjd6.W.dict.yaml",
+       "pinyin_simp.dict.yaml",
+       "pinyin_simp.schema.yaml"
+    }
+    for _, d in ipairs(dicts) do
+       ctx:download_file(d, d)
+    end
+    
     ctx:download_file("xmjd6.symbols.yaml", "xmjd6.symbols.yaml")
-    ctx:download_dir("xmjd6", "xmjd6")
-    -- ctx:download_dir("opencc", "opencc") 
-    local dict_files = {
-        "xmjd6.extended.dict.yaml",
-        "xmjd6.cx.dict.yaml",    
-        "xmjd6.gbk.dict.yaml",
-        "liangfen.dict.yaml",
-        "xmjd6.Y.dict.yaml",
-        "xmjd6.Z.dict.yaml",
-        "xmjd6.W.dict.yaml",
-    }
 
-    for _, file in ipairs(dict_files) do
-        ctx:download_file(file, file)
-    end
-
-    local dep_schemas = {
-        "pinyin_simp.schema.yaml",
-        "pinyin_simp.dict.yaml"
-    }
-    for _, file in ipairs(dep_schemas) do
-        ctx:download_file(file, file)
-    end
 end
 
 return M
